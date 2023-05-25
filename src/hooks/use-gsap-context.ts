8ebcf2f92a3ee72653154927061cf5863ff74328
David Khierl <dkrgidor@gmail.com>
2023-05-25 21:16:08 +0400

    chore: update packages

--- src/hooks/use-gsap-context.ts
+++ src/hooks/use-gsap-context.ts
@@ -1,6 +1,18 @@
 import { gsap } from 'gsap'
 import { MutableRefObject, useMemo } from 'react'
+import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect'
 
-export function useGsapContext<T = any>(scope: MutableRefObject<T>) {
-  return useMemo(() => gsap.context(() => {}, scope), [scope])
+export function useGsapContext<T = any>(
+  scope: MutableRefObject<T>,
+  autoRevert = true
+) {
+  const context = useMemo(() => gsap.context(() => {}, scope), [scope])
+
+  useIsomorphicLayoutEffect(() => {
+    return () => {
+      if (autoRevert) context.revert()
+    }
+  }, [context])
+
+  return context
 }
